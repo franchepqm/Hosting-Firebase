67b19c3dc7fa4ab1e9edbd5cc6bd7814afdf0d99
franchepqm_ <74469603+franchepqm@users.noreply.github.com>
2024-05-08 20:42:16 -0300

    Hosting y Dominio Gratis
    youtube.com/@franchepqm

---
+++ firebaseconect.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
